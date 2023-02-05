4fdea4c155a749f3485bae051d1b5f26a9847bcf
Brandon Valley <bavvh8@mst.edu>
2023-02-05 12:29:13 -0600

    whitespace

--- audio_edit_utils.py
+++ audio_edit_utils.py
@@ -8,9 +8,9 @@ from pydub import AudioSegment
 def get_audio_from_video(in_vid_path, out_audio_path):
     cmd = 'ffmpeg -i ' + in_vid_path + ' -acodec pcm_s16le -ac 2 ' + out_audio_path + ' -y'
     subprocess.call(cmd, shell = False)
-    
-    
-'''takes .wav file, no spaces in path'''    
+
+
+'''takes .wav file, no spaces in path'''
 def get_audio_duration(in_audio_path):
     cmd = 'ffprobe -i ' + in_audio_path + ' -show_entries format=duration -v quiet -of csv="p=0"'
     d = subprocess.check_output(cmd, shell = False)
@@ -25,10 +25,10 @@ def clip_audio(in_audio_path, clipped_audio_path, start_time, end_time):
 
 # returns False if there is no speech in audio
 def transcribe_audio(in_audio_path):
-    # use the audio file as the audio source                                        
+    # use the audio file as the audio source
     r = sr.Recognizer()
     with sr.AudioFile(in_audio_path) as source:
-            audio = r.record(source)  # read the entire audio file                  
+            audio = r.record(source)  # read the entire audio file
     try:
         return r.recognize_google(audio)
     except sr.UnknownValueError:
